92efb854615b17612f338e08c7b0c8a50415dc08
ruskistandard <jankatalinic@gmail.com>
2022-01-10 16:59:02 +0000

    Reads upload

--- Pre-filter/pre-filter_LRRs.sh
+++ Pre-filter/pre-filter_LRRs.sh
@@ -2,15 +2,16 @@
 
 # '*' is a wildcard character in bash. It tells the shell to look for files which end in .fastq
 
+#WORKDIR=/Users/jan/FAKS/PhD/LRR_project/Data/NANOPORE/RAW_DATA
 cd /Users/jan/FAKS/PhD/LRR_project/Data/NANOPORE/RAW_DATA
 
 for f in *.fastq
 do
 	echo " ------------------------- Filtering and mapping... - $f 	----------------------------------"
-	
-	nanofilt --length 4100 --maxlength 4700 $f > $f.lengthfilt.fastq
 
-	gsed -n '1~4s/^@/>/p;2~4p' $f.lengthfilt.fastq > $f.lengthfilt.fasta
+	nanofilt --length 4100 --maxlength 4700 $f > nanofilt/$f.lenfilt.fastq
+
+	gsed -n '1~4s/^@/>/p;2~4p' nanofilt/$f.lenfilt.fastq > nanofilt/$f.lenfilt.fasta
 
 
 done
